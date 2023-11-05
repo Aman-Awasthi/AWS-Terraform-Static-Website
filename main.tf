423170cf73097b1a61e11573ce2855827274fd81
admin-at-659152883721 <amana6420@gmail.com>
2023-11-05 17:04:27 +0530

    Terraform-AWS-Static-Website

---
+++ main.tf
@@ -0,0 +1,80 @@
+# Creating s3 bucket
+resource "aws_s3_bucket" "mybucket" {
+  bucket = var.bucket
+
+  tags = {
+    Name        = "Static-Website"
+    Environment = "Dev"
+  }
+}
+
+#uploading files to bucket
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "index.html"
+  source       = "index.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+#uploading files to bucket
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "error.html"
+  source       = "error.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+#uploading files to bucket
+resource "aws_s3_object" "image" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "profile.jpg"
+  source = "profile.jpg"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "accessblock" {
+
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+# Ownership of the bucket
+resource "aws_s3_bucket_ownership_controls" "bucketowner" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# To expose bucket objects become publicly accessible.
+resource "aws_s3_bucket_acl" "aclcontrol" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucketowner,
+    aws_s3_bucket_public_access_block.accessblock
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
+# Static Website configuration
+resource "aws_s3_bucket_website_configuration" "myportfoliowebsite" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [aws_s3_bucket_acl.aclcontrol]
+}
